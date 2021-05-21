bb146bf317e2d340296a2641e27877c05ee07ae7
WenyuanShao <shaowy@gwmail.gwu.edu>
2021-05-21 15:03:59 -0500

    push a version of edf_scheduler for review

--- src/components/lib/sl/sl_mod_edf_modified.c
+++ src/components/lib/sl/sl_mod_edf_modified.c
@@ -2,6 +2,7 @@
 #include <sl_consts.h>
 #include <sl_mod_policy.h>
 #include <sl_plugins.h>
+#include <bitmap.h>
 
 #ifdef SL_MOD_DEBUG
 #define debug printc
@@ -9,20 +10,110 @@
 #define debug(fmt,...)
 #endif
 
-#define SL_EDF_MAX_DEADLINE ~(cycles_t)0
-#define SL_WINDOW_SZ        1024
-#define SL_WORD_SZ          32
-#define INTERVAL_SZ         (2700 * 1000)
+#define DEADLINE_QUANTUM_LEN (500 * 2100)
+#define LVL1_BITMAP_SZ       1
+#define LVL2_BITMAP_SZ       (32)
+#define DEADLINE_WINDOW_SZ   (LVL2_BITMAP_SZ * 32)
+#define DEADLINE_MAX         ((DEADLINE_WINDOW_SZ - 1) * DEADLINE_QUANTUM_LEN)
 
-struct base_info {
-	unsigned long long base;
-	int                head;
-};
+struct sched_bitmaps {
+	u64_t now_quantized;
+	u32_t lvl1[LVL1_BITMAP_SZ];
+	u32_t lvl2[LVL2_BITMAP_SZ];
+	struct ps_list_head runqs[DEADLINE_WINDOW_SZ];
+	struct ps_list_head low_prio_rq;
+} __attribute__((aligned(64)));
 
-volatile unsigned long bm_lv2[NUM_CPU][SL_WORD_SZ];
-volatile unsigned long bm_lv1[NUM_CPU];
-struct ps_list_head threads[NUM_CPU][SL_WINDOW_SZ] CACHE_ALIGNED;
-volatile struct base_info base_info[NUM_CPU];
+struct sched_bitmaps runqueues[NUM_CPU];
+
+static inline void
+bm_print(unsigned long input)
+{
+	int b[32];
+	int i = 0, count = 0;
+
+	printc("0x%lx------->", input);
+	for (i = 0; i < 32; i++) {
+		b[i] = input % 2;
+		input /= 2;
+	}
+	i = 32;
+	while (i) {
+		printc("%u", b[--i]);
+		if (++count % 4 ==  0 && i){
+			printc(" ");
+			count = 0;
+		}
+	}
+	printc("\n");
+}
+
+static inline u64_t
+curr_offset(void)
+{
+	return ps_tsc() / DEADLINE_QUANTUM_LEN;
+}
+
+void
+bitmap_edf_update_offsets(struct sched_bitmaps *rq)
+{
+	struct sl_thd_policy *node, *t = NULL;
+	u64_t current_offset = curr_offset();
+	u64_t now_offset;
+	int i = 0;
+
+	unsigned long long start, end;
+
+	for (now_offset = rq->now_quantized; now_offset < current_offset; now_offset++) {
+		assert(rq->now_quantized < current_offset);
+		unsigned long now  = now_offset % DEADLINE_WINDOW_SZ;
+		unsigned long next = (now_offset + 1) % DEADLINE_WINDOW_SZ;
+
+		if (!ps_list_head_empty(&rq->runqs[now])) {
+
+			ps_list_foreach_d(&rq->runqs[now], node) {
+				assert(node->prio_idx == (int)now);
+				node->prio_idx = (int)next;
+				assert(sl_mod_thd_get(node)->state == SL_THD_RUNNABLE);
+			}
+
+			ps_list_head_append_all_d(&rq->runqs[next], &rq->runqs[now]);
+			assert(!ps_list_head_empty(&rq->runqs[next]));
+			assert(ps_list_head_empty(&rq->runqs[now]));
+			bitmap_unset(rq->lvl2, now);
+			if (rq->lvl2[now/LVL2_BITMAP_SZ] == 0) bitmap_unset(rq->lvl1, now/LVL2_BITMAP_SZ);
+			bitmap_set(rq->lvl2, next);
+			bitmap_set(rq->lvl1, next/LVL2_BITMAP_SZ);
+		}
+	}
+	rq->now_quantized = current_offset;
+}
+
+u32_t rotr32(u32_t value, unsigned int rotation)
+{
+	return value >> rotation | value << (32 - rotation);
+}
+
+unsigned int
+first_bit_lvl1(u32_t lvl1, unsigned int rotation)
+{
+	u32_t rotated;
+	unsigned int ret;
+
+	assert(rotation < 32);
+	rotated = rotr32(lvl1, rotation);
+
+	ret = __builtin_ctzl(rotated);
+	if (ret < (LVL2_BITMAP_SZ - rotation))
+		ret += rotation;
+	else
+		ret -= (LVL2_BITMAP_SZ - rotation);
+	/*if (ret>=32) {
+		printc("\trotat: %d----->", rotation);
+		bm_print(rotated);
+	}*/
+	return ret;
+}
 
 static inline int
 __find_first_bit(unsigned long bitmap)
@@ -31,193 +122,155 @@ __find_first_bit(unsigned long bitmap)
 }
 
 static inline void
-__set_bit(unsigned long *bitmap, int pos)
+__check_list(struct sched_bitmaps* rq, int pos)
 {
-	assert(pos < SL_WORD_SZ && pos >= 0);
-	*bitmap |= (0x1 << pos);
-	return;
-}
-
-static inline int
-__verify_bit(unsigned long *bitmap, int pos)
-{
-	unsigned long ret;
-	assert(pos < SL_WORD_SZ && pos >= 0);
-	ret = *bitmap & (0x1 << pos);
-	if (ret == (unsigned long)0U)
-		return 0;
-	else
-		return 1;
-}
-
-static inline void
-__clear_bit(unsigned long *bitmap, int pos)
-{
-	assert(pos < SL_WORD_SZ && pos >= 0);
-	*bitmap &= ~(0x1 << pos);
-	return;
-}
-
-static inline void
-__check_clear(int l_pos)
-{
-	int cpu = cos_cpuid();
-	int bm_pos = 0;
-
-	assert(l_pos >= 0 & l_pos < 1024);
-
-	if (l_pos >= base_info[cpu].head)
-		bm_pos = l_pos - base_info[cpu].head;
-	else
-		bm_pos = l_pos - base_info[cpu].head + SL_WINDOW_SZ;
-	if (ps_list_head_empty(&threads[cpu][l_pos])) {
-		__clear_bit(&bm_lv2[cpu][bm_pos/SL_WORD_SZ], bm_pos%SL_WORD_SZ);
-		if (!bm_lv2[cpu][pos/SL_WORD_SZ])
-			__clear_bit(&bm_lv1[cpu], bm_pos/SL_WORD_SZ);
+	if (ps_list_head_empty(&rq->runqs[pos])) {
+		bitmap_unset(rq->lvl2, pos);
+		if (rq->lvl2[pos/LVL2_BITMAP_SZ] == 0) bitmap_unset(rq->lvl1, pos/LVL2_BITMAP_SZ);
 	}
-	return;
+}
+
+thdid_t
+sl_mod_gettid(struct sl_thd_policy *t)
+{
+	struct sl_thd *thd;
+	thdid_t        tid;
+	
+	assert(t);
+	thd = sl_mod_thd_get(t);
+	assert(thd);
+	tid = sl_thd_thdid(thd);
+	assert(tid);
+
+	return tid;
 }
 
 void
 sl_mod_execution(struct sl_thd_policy *t, cycles_t cycles)
 { }
 
-unsigned long long previous[NUM_CPU];
-volatile int pcnt = 1;
-
 struct sl_thd_policy *
 sl_mod_schedule(void)
 {
-    int lv1_idx, lv2_idx, idx, cpu = cos_cpuid();
-    struct sl_thd_policy *thd = NULL;
-	struct sl_thd_policy *node, *t = NULL;
-	unsigned long long now;
-	int head, legacy = 0;
-	int i = 0, cnt = 0;
+    struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	struct sl_thd_policy *thd;
+	unsigned int rotation, tid;
+	unsigned int first_lvl1, first_lvl2, pos;
+	u32_t lvl2;
+	struct ps_list_head *rq_head;
+	int i= 0;
 
-	now = ps_tsc();
-
-	/* If INTERVAL_SZ greater than a timer-tick, this while loop can be replaced by an if */
-	while (now > (base_info[cpu].base + INTERVAL_SZ)) {
-		head = base_info[cpu].head;
-		if (!ps_list_head_empty(&threads[cpu][head])) {
-			ps_list_foreach_del_d(&threads[cpu][head], node, t) {
-				ps_list_rem_d(node);
-				ps_list_head_add_d(&threads[cpu][head+1], node);
-				
-				assert(node->prio_idx == head);
-				node->prio_idx = (head+1)&(SL_WINDOW_SZ-1);
-				assert(!ps_list_head_empty(&threads[cpu][head+1]));
-			}
-			legacy = 1;
+	bitmap_edf_update_offsets(rq);
+	if (rq->lvl1[0] == (unsigned long)0) {
+		if (ps_list_head_empty(&rq->low_prio_rq)) {
+			if (cos_cpuid() == 4)
+				printc("!");
+			return NULL;
+		} else {
+			return ps_list_head_first_d(&rq->low_prio_rq, struct sl_thd_policy);
 		}
-		base_info[cpu].head ++;
-		base_info[cpu].head &= (SL_WINDOW_SZ - 1);
-		base_info[cpu].base += INTERVAL_SZ;
-		cnt++;
 	}
 
-	/* Reconstruct bitmap after moving the head of the list array */
-	if (likely(cnt)) {
-		assert(cnt < 32);
-		bm_lv1[cpu] = (unsigned long)0U;
+	rotation = rq->now_quantized % DEADLINE_WINDOW_SZ;
+	first_lvl1 = first_bit_lvl1(rq->lvl1[0], rotation/LVL2_BITMAP_SZ);
+	first_lvl2 = __builtin_ctzl(rq->lvl2[first_lvl1]);
+	pos = (first_lvl1 * LVL2_BITMAP_SZ) + first_lvl2;
+	assert(pos < 1024);
+	rq_head = &rq->runqs[pos];
 
-		for (i = 0; i < SL_WORD_SZ-1; i++) {
-			bm_lv2[cpu][i] = (bm_lv2[cpu][i+1] << (SL_WORD_SZ - cnt)) | (bm_lv2[cpu][i] >> cnt);
-			if (bm_lv2[cpu][i])	__set_bit(&bm_lv1[cpu], i);
-		}
-		bm_lv2[cpu][SL_WORD_SZ-1] = bm_lv2[cpu][SL_WORD_SZ-1] >> cnt;
-		bm_lv2[cpu][0] |= (unsigned long)legacy;
-		if(ps_list_head_empty(&threads[cpu][base_info[cpu].head]) != legacy) {
-			printc("ERROR: %d\n", legacy);
-		}
-
-		if (bm_lv2[cpu][SL_WORD_SZ-1])	__set_bit(&bm_lv1[cpu], SL_WORD_SZ-1);
-		bm_ver[cpu] = bm_lv1[cpu];
-	}
-
-	if (bm_lv1[cpu] == 0) {
-		for (i = 0; i < SL_WORD_SZ; i++) {
-			assert(!bm_lv2[cpu][i]);
-		}
+	if(!ps_list_head_empty(rq_head)) {
+		thd = ps_list_head_first_d(rq_head, struct sl_thd_policy);
+	} else {
+		__check_list(rq, pos);
 		return NULL;
 	}
-
-	lv1_idx = __find_first_bit(bm_lv1[cpu]);
-	lv2_idx = __find_first_bit(bm_lv2[cpu][lv1_idx]);
-	idx = lv1_idx*SL_WORD_SZ + lv2_idx + base_info[cpu].head;
-	idx &= (SL_WINDOW_SZ-1);
-	
-	if (!ps_list_head_empty(&threads[cpu][idx])) {
-		thd = ps_list_head_first_d(&threads[cpu][idx], struct sl_thd_policy);
-		assert(thd);
-		assert(thd->prio_idx == idx);
-		return thd;
-	}
-	assert(0);
-
-	return NULL;
+	assert(thd->prio_idx == pos);
+	return thd;
 }
 
 void
 sl_mod_block(struct sl_thd_policy *t)
 {
-	int cpu = cos_cpuid();
-	int pos;
-	
-	pos = t->prio_idx;
-	assert((pos < SL_WINDOW_SZ) && (pos >= 0));
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	int pos = t->prio_idx;
 
+	bitmap_edf_update_offsets(rq);
 	ps_list_rem_d(t);
 
-	__check_clear(pos);
+	if (pos != -1) {
+		__check_list(rq, pos);
+		t->prio_idx = -1;
+	}
 	sl_mod_thd_get(t)->state = SL_THD_BLOCKED;
+	bitmap_edf_update_offsets(rq);
+	assert(t->prio_idx == -1);
 }
 
 void
 sl_mod_wakeup(struct sl_thd_policy *t)
 {
-	int cpu = cos_cpuid();
-	int l_pos, bm_pos;
-	unsigned long long now;
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	unsigned long long dl_quantized = (t->deadline / DEADLINE_QUANTUM_LEN);
+	unsigned long long now_quantized = 0;
+	int pos = t->prio_idx;
 
+	bitmap_edf_update_offsets(rq);
+	now_quantized = rq->now_quantized;
 	assert(t->deadline);
-	assert(t->deadline < SL_EDF_MAX_DEADLINE);
-	
-	if (t->deadline < base_info[cpu].base) {
-		bm_pos = 0;
-	} else {
-		bm_pos = (int)(t->deadline - base_info[cpu].base)/INTERVAL_SZ;
+	assert(sl_mod_thd_get(t)->wakeup_cnt > 0);
+
+	ps_list_rem_d(t);
+	if (dl_quantized < now_quantized) {
+	/* if already passed its deadline when wakeup, let it exectue once */
+		dl_quantized = now_quantized;
 	}
-	assert((bm_pos < SL_WINDOW_SZ) && (bm_pos >= 0));
-	l_pos = (bm_pos + base_info[cpu].head) & (SL_WINDOW_SZ - 1);
-	t->prio_idx = l_pos;
+	assert(dl_quantized < (now_quantized + DEADLINE_WINDOW_SZ));
 
 	/* Wakeup a thread which is already active after modifying its deadline */
-	ps_list_rem_d(t);
-	__check_clear(l_pos);
+	if (pos >= 0) {
+		__check_list(rq, pos);
+	}
+	dl_quantized %= DEADLINE_WINDOW_SZ;
+	assert(t->prio_idx == -1);
+	t->prio_idx = dl_quantized;
 
-	ps_list_head_add_d(&threads[cpu][l_pos], t);
-	__set_bit(&bm_lv2[cpu][bm_pos/SL_WORD_SZ], bm_pos%SL_WORD_SZ);
-	__set_bit(&bm_lv1[cpu], bm_pos/SL_WORD_SZ);
-
+	ps_list_head_append_d(&rq->runqs[dl_quantized], t);
+	assert(!ps_list_head_empty(&rq->runqs[dl_quantized]));
+	bitmap_set(rq->lvl2, dl_quantized);
+	bitmap_set(rq->lvl1, dl_quantized/LVL2_BITMAP_SZ);
 }
 
 void
 sl_mod_yield(struct sl_thd_policy *t, struct sl_thd_policy *yield_to)
 {
-	assert(t->prio_idx >=0 && t->prio_idx < SL_WINDOW_SZ);
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	bitmap_edf_update_offsets(rq);
+	int pos = t->prio_idx;
+
 	ps_list_rem_d(t);
-	ps_list_head_append_d(&threads[cos_cpuid()][t->prio_idx], t);
+	if (t->deadline < ps_tsc() || pos == -1) {
+		t->prio_idx = -1;
+		__check_list(rq, pos);
+		ps_list_head_append_d(&rq->low_prio_rq, t);
+		return;
+	}
+	assert(t->prio_idx >= 0);
+	assert(t->prio_idx < DEADLINE_WINDOW_SZ);
+	ps_list_head_append_d(&rq->runqs[pos], t);
+	bitmap_set(rq->lvl2, pos);
 }
 
 void
 sl_mod_thd_create(struct sl_thd_policy *t)
 {
-    t->period      = 0;
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	unsigned long long tmp = (unsigned long long)500 * (unsigned long long)1000 * (unsigned long long)2100;
+
+	t->period      = 0;
     t->period_usec = 0;
-    t->priority    = t->deadline = SL_EDF_MAX_DEADLINE;
+   	t->priority = t->deadline = ps_tsc() + tmp;
 	t->prio_idx    = -1;
+	t->deadline    = 0;
     
 	ps_list_init_d(t);
 }
@@ -225,42 +278,56 @@ sl_mod_thd_create(struct sl_thd_policy *t)
 void
 sl_mod_thd_delete(struct sl_thd_policy *t)
 {
-	int cpu = cos_cpuid();
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+
+	assert(0);
 
 	ps_list_rem_d(t);
-
-	__check_clear(t->prio_idx);
-	t->prio_idx = -1;
+	__check_list(rq, t->prio_idx);
 }
 
 void
 sl_mod_thd_param_set(struct sl_thd_policy *t, sched_param_type_t type, unsigned long long v)
 {
-	int cpu = cos_cpuid();
-	int i = 0, last;
-	
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	int i = 0, dl_quantized;
 	unsigned long long pos;
+	unsigned long long curr;
 
+	bitmap_edf_update_offsets(rq);
 	switch (type) {
 		case SCHEDP_PRIO:
 		{
-			t->deadline = SL_EDF_MAX_DEADLINE;
-			last = (SL_WINDOW_SZ - 1 + base_info[cpu].head) & (SL_WINDOW_SZ - 1);
+			assert(cos_cpuid() == 0);
+			t->priority = v;
+			sl_thd_setprio(sl_mod_thd_get(t), t->priority);
 			ps_list_rem_d(t);
-			/*
-			 * FIXME: the normal path should be modified as always call thd wakeup after set param. 
-			 * Here we wake it up after setting the parameter.
-			 */
-			ps_list_head_append_d(&threads[cpu][last], t);
-			__set_bit(&bm_lv2[cpu][last/SL_WORD_SZ], last%SL_WORD_SZ);
-			__set_bit(&bm_lv1[cpu], last/SL_WORD_SZ);
+			dl_quantized = DEADLINE_WINDOW_SZ - 1 + rq->now_quantized;
+			t->deadline = dl_quantized * DEADLINE_QUANTUM_LEN;
+			ps_list_rem_d(t);
+			assert(t->prio_idx == -1);
+			__check_list(rq, t->prio_idx);
+
+			/* FIXME: the normal path should be modified as always call thd wakeup after set param. */
+			ps_list_head_append_d(&rq->runqs[DEADLINE_WINDOW_SZ-1], t);
+			t->prio_idx = DEADLINE_WINDOW_SZ-1;
+			bitmap_set(rq->lvl2, t->prio_idx);
+			bitmap_set(rq->lvl1, t->prio_idx/LVL2_BITMAP_SZ);
+			assert(cos_cpuid() == 0);
 			break;
 		}
 		case SCHEDP_PRIO_BLOCK:
 		{
+			/* should not be used */
+			assert(0);
+			/*t->priority = v;
+			sl_thd_setprio(sl_mod_thd_get(t), t->priority);
 			sl_mod_thd_get(t)->state = SL_THD_BLOCKED;
-			t->deadline = SL_EDF_MAX_DEADLINE;
-			break;
+			dl_quantized = DEADLINE_WINDOW_SZ - 1 + rq->now_quantized;
+			t->deadline = dl_quantized * DEADLINE_QUANTUM_LEN;
+			sl_mod_thd_get(t)->state = SL_THD_BLOCKED;
+			t->prio_idx = -1;
+			break;*/
 		}
 		case SCHEDP_WINDOW:
 		{
@@ -269,12 +336,16 @@ sl_mod_thd_param_set(struct sl_thd_policy *t, sched_param_type_t type, unsigned 
 		}
 		case SCHEDP_DEADLINE:
 		{
+			sl_mod_thd_get(t)->state = SL_THD_BLOCKED;
 			t->deadline = v;
-			if(sl_mod_thd_get(t)->state != SL_THD_BLOCKED) {
-				pos = (t->deadline - base_info[cpu].base)/INTERVAL_SZ + base_info[cpu].head;
-				assert((pos >= 0) & (pos < SL_EDF_MAX_DEADLINE));
-				pos &= (SL_WINDOW_SZ - 1);
+			curr = ps_tsc();
+			unsigned long long tmp = (unsigned long long)500 * (unsigned long long)1000 * (unsigned long long)2100;
+			if (v > curr+tmp) {
+				printc("deadline: %llu, MAX: %llu\n", v, ~(cycles_t)0);
 			}
+			assert(v <= curr + tmp);
+			t->prio_idx = -1;
+
 			debug("param_set= adjust idx: %d, deadline: %llu\n", t->prio_idx, t->deadline);
 			break;
 		}
@@ -289,16 +360,13 @@ sl_mod_thd_param_set(struct sl_thd_policy *t, sched_param_type_t type, unsigned 
 void
 sl_mod_init(void)
 {
-	int i, cpu = cos_cpuid();
+	struct sched_bitmaps *rq = &runqueues[cos_cpuid()];
+	int i;
 
-	memset(threads[cpu], 0, sizeof(struct ps_list_head) * SL_WINDOW_SZ);
-	memset(&base_info[cpu], 0, sizeof(struct base_info));
-	memset(bm_lv2[cpu], 0, sizeof(unsigned long) * SL_WORD_SZ);
-	memset(&bm_lv1[cpu], 0, sizeof(unsigned long));
-	
-	base_info[cpu].base = ps_tsc();
-
-	for (i = 0; i < SL_WINDOW_SZ; i++) {
-		ps_list_head_init(&threads[cpu][i]);
+	memset(rq, 0, sizeof(struct sched_bitmaps));
+	rq->now_quantized = curr_offset();
+	ps_list_head_init(&rq->low_prio_rq);
+	for (i = 0; i < DEADLINE_WINDOW_SZ; i++) {
+		ps_list_head_init(&rq->runqs[i]);
 	}
 }
